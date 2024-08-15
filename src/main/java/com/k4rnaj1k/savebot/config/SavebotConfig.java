1260c227329758b5b60cd1c2567e04488e622a69
k4rnaj1k <vladliasota@gmail.com>
2024-08-16 01:48:45 +0300

    Add support for specifying cobalt api url

--- src/main/java/com/k4rnaj1k/savebot/config/SavebotConfig.java
+++ src/main/java/com/k4rnaj1k/savebot/config/SavebotConfig.java
@@ -1,5 +1,6 @@
 package com.k4rnaj1k.savebot.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,7 +8,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class SavebotConfig {
     @Bean
-    public WebClient cobaltWebClient() {
-        return WebClient.builder().baseUrl("https://api.cobalt.tools").build();
+    public WebClient cobaltWebClient(@Value("${savebot.cobalt.api.url}") String cobaltApi) {
+        return WebClient.builder().baseUrl(cobaltApi).build();
     }
 }
